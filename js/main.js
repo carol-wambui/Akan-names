40c4b46f2de8f38baeb827623d0577fcd1250461
carol-wambui <carolwambuidaystar@gmail.com>
2020-10-03 21:57:06 +0300

    modify the function

--- js/main.js
+++ js/main.js
@@ -2,15 +2,14 @@ var akanNameMale=["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
 var akanNameFemale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"]
 
 function ghana(){
-    var centuary=parseInt(document.getElementById("centuary").value);
-    var year=parseInt(document.getElementById("year").value);
-    var month=parseInt(document.getElementById("month").value);
-    var day=parseInt(document.getElementById("day").value);
-    var gender=document.getElementById("gender").value;
+   
 
+    var CC=centuary;
+    var YY=year;
+    var MM=month;
+    var DD=day;
 
-
-    
+    var d = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
     
 
 
