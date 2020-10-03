85c7974d808c8d792c3ab36f162d468c12da6f5f
carol-wambui <carolwambuidaystar@gmail.com>
2020-10-03 22:00:33 +0300

    add id's from index.html to js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ var akanNameMale=["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
 var akanNameFemale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"]
 
 function ghana(){
-   
+    var centuary=parseInt(document.getElementById("centuary").value);
+    var year=parseInt(document.getElementById("year").value);
+    var month=parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("day").value);
+    var gender=document.getElementById("gender").value;
 
     var CC=centuary;
     var YY=year;
