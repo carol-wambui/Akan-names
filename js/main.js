b51a0587694a162231312c28faf3053255d8ab0a
carol-wambui <carolwambuidaystar@gmail.com>
2020-10-03 21:39:05 +0300

    add arrays showing akan names

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var akanNameMale=["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
+var akanNameFemale=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"]
+
+function ghana(){
+    var centuary=parseInt(document.getElementById("centuary").value);
+    var year=parseInt(document.getElementById("year").value);
+    var month=parseInt(document.getElementById("month").value);
+    var day=parseInt(document.getElementById("day").value);
+    var gender=document.getElementById("gender").value;
+
+
+
+    
+    
+
+
+
+
+}
